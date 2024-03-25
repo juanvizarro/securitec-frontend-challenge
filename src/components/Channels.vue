66c72aac40235a04c4e559f5cef8cc49883edeaf
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2024-03-25 07:58:50 -0500

    feat: show contact channels

---
+++ src/components/Channels.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+    import { computed } from 'vue'
+    import type { ContactChannel } from '@/interfaces/IContact';
+
+    const props = defineProps<{
+        items: ContactChannel[]
+    }>()
+
+    const uniqueChannels = computed(() => props.items.filter((item, index, self) => index === self.findIndex(selfItem => (selfItem.id === item.id))))
+</script>
+<template>
+    <div class="channels">
+        <img class="channel-icon" v-for="(channel, i) in uniqueChannels" :key="`channel_${i}_${channel.id}`" :src="channel.icon" />
+    </div>
+</template>
+<style lang="scss" scoped>
+.channels{
+    display: flex;
+    gap: 6px;
+    .channel-icon{
+        width: 14px;
+        height: 14px;
+    }
+}
+</style>
