7fd08f6a606bbfc4de8705edca1b85bfe6fdb6bf
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2024-03-26 08:42:54 -0500

    feat: create contact queues

---
+++ src/components/contact/ContactQueues.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    import { computed } from 'vue'
+
+    const props = defineProps<{
+        items: string[]
+    }>()
+
+    const text = computed(() => {
+        const length = props.items?.length ?? 0
+        return length === 0 ? 'sin cola' : length > 1 ? `${length} cola` : `${length} colas`
+    })
+</script>
+<template>
+    <a-popover :title="text">
+        <template #content>
+        <p v-for="item in props.items" :key="`queue_${item}`">{{ item }}</p>
+        </template>
+        <a-button type="link">
+            {{ text }}
+        </a-button>
+    </a-popover>
+</template>
