03fb992f2c59aac3c4fc317cda729bedd60eab1f
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2024-03-26 08:41:10 -0500

    feat: add contact interfaces

--- src/interfaces/IContact.ts
+++ src/interfaces/IContact.ts
@@ -37,4 +37,69 @@ export interface ContactChannel {
     id: number
     name: string
     icon: string
+    status: number
+}
+
+export interface ContactDetail extends Omit<Contact,'channels'> {
+    channels: ContactChannelDetail[]
+    operation_type: number
+    last_interaction: {
+        channel: ContactChannel
+        identifier: string
+    }
+    last_interaction_date: string
+    created_at: string
+    updated_at: string
+}
+
+export interface ContactChannelDetail {
+    id: number
+    identifier: string
+    type_interaction: number
+    channel_customer: {
+        id: number
+        indentifier: string
+        alias: string
+        data_parameters: {
+            city: number
+            email: string
+            apikey: string
+            app_id: string
+            address: string
+            country: number
+            sc_user: {
+                id: number
+                fullname: string
+            }
+            website: string
+            app_name: string
+            code_phone: number
+            description: string
+            picture_src: string
+            company_name: string
+            phone_number: string
+            url_callback: string
+            type_industry: number
+            company_language: string
+            facebook_page_whatsapp: string
+        }
+    },
+    channel: ContactChannel
+    state: number
+    code_country: string
+    queue: number
+    blocked?: boolean
+    call_duration?: number
+    contact_date?: string
+}
+export interface ContactFormState {
+    name: string
+    paternal_surname: string
+    maternal_surname: string
+    type_document: number
+    document_number: string
+    address: string
+    email: string
+    phone: string
+    code_phone: string
 }
