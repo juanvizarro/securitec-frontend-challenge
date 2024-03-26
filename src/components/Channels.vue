43bf6e4c3c99cef4b5a17d141a6fa3538a877aba
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2024-03-26 08:42:28 -0500

    feat: add popover on channels

--- src/components/Channels.vue
+++ src/components/Channels.vue
@@ -6,17 +6,24 @@
         items: ContactChannel[]
     }>()
 
-    const uniqueChannels = computed(() => props.items.filter((item, index, self) => index === self.findIndex(selfItem => (selfItem.id === item.id))))
+    const uniqueChannels = computed(() => props.items.filter((item, index, self) => index === self.findIndex(selfItem => (selfItem.id === item.id))).map(channel => ( {...channel } )))
 </script>
 <template>
     <div class="channels">
-        <img class="channel-icon" v-for="(channel, i) in uniqueChannels" :key="`channel_${i}_${channel.id}`" :src="channel.icon" />
+        <a-popover :title="channel.name" v-for="(channel, i) in uniqueChannels" :key="`channel_${i}_${channel.id}`">
+            <template #content>
+                <ul>
+                    <!-- TODO: missing phone values -->
+                </ul>
+            </template>
+            <img class="channel-icon" :src="channel.icon" />
+        </a-popover>
     </div>
 </template>
 <style lang="scss" scoped>
 .channels{
     display: flex;
-    gap: 6px;
+    gap: 8px;
     .channel-icon{
         width: 14px;
         height: 14px;
