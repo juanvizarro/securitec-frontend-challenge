b374af7c7f8abc113c298f2c9a4e2ad477ee2b93
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2024-03-26 08:47:04 -0500

    feat: show contact detail with drawer

--- src/components/contact/ContactDatatable.vue
+++ src/components/contact/ContactDatatable.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
+    import { computed, ref, h } from 'vue'
     import Channels from '@/components/Channels.vue'
-    import { DeleteOutlined } from '@ant-design/icons-vue'
-    import { STable, type STableProps } from '@surely-vue/table'
+    import ContactQueues from '@/components/contact/ContactQueues.vue'
+    import ContactDetailDrawer from '@/components/contact/drawers/ContactDetailDrawer.vue'
+    import { DeleteOutlined, ExclamationCircleOutlined } from '@ant-design/icons-vue'
+    import { ContactService } from '@/services/contact.service'
+    import { STable } from '@surely-vue/table'
+    import { Modal, message } from 'ant-design-vue'
+    import { getDate } from '@/helpers/date.helper'
     import type { Contact } from '@/interfaces/IContact'
-
-    type Column = STableProps['columns']
+    import type { Column } from '@/interfaces/GenericTypes'
 
     const props = defineProps<{
         data: Contact[]
     }>()
+
+    const [modal, contextHolder] = Modal.useModal()
+    const contactService = new ContactService()
 
     const columns = <Column>[
         {
@@ -21,7 +29,7 @@
         {
             title: 'Nombres y Apellidos',
             dataIndex: 'full_name',
-            width: 200,
+            width: 250,
             align: 'left'
         },
         {
@@ -106,7 +114,7 @@
             title: 'Usuario asignado',
             dataIndex: 'user_assigned',
             align: 'left',
-            width: 180
+            width: 220
         },
         {
             title: 'Fecha de creación',
@@ -127,9 +135,54 @@
             width: 100
         },
     ]
-
+    
+    const showContactDetailModal = ref(false)
+    const contactDetail = ref<Contact|null>(null)
+    const activeContactDetailTab = ref<'info'|'phones'>('info')
     function showContactDetail(record: Contact, tab: 'info'|'phones'){
-        console.log(record, tab)
+        contactDetail.value = record
+        activeContactDetailTab.value = tab
+        setTimeout(() => {
+            showContactDetailModal.value = true
+        }, 200)
+    }
+    const contactDetailIndex = computed(() => props.data.findIndex(item => item.id === contactDetail.value?.id))
+    function onPreviousDetailContact(){
+        if(contactDetailIndex.value < 0) return
+
+        contactDetail.value = props.data[contactDetailIndex.value-1]
+    }
+    function onNextDetailContact(){
+        if(contactDetailIndex.value >= (props.data.length - 1)) return
+
+        contactDetail.value = props.data[contactDetailIndex.value+1]
+    }
+
+    function showDeleteContactModal(id: number, full_name: string) {
+        modal.confirm({
+            title: `¿Estás seguro de eliminar el contacto ${full_name}?`,
+            icon: h(ExclamationCircleOutlined),
+            content: 'Recuerda que ya no se visualizará este contacto en la tabla. Si deseas volver a visualizarlo, deberás agregarlo nuevamente a Score.',
+            okText: 'Eliminar',
+            okType: 'primary',
+            okButtonProps: {
+                danger: true
+            },
+            cancelText: 'Cancelar',
+            async onOk() {
+                try {
+                    const response = await contactService.deleteContact(id)
+                    message.success({
+                        content: () => `Se ha eliminado el contacto “${full_name}” correctamente`,
+                        class: 'confirm-notification'
+                    })
+                    return response
+                } catch {
+                    return console.error('error in contact service');
+                }
+            },
+            onCancel(){}
+        });
     }
 </script>
 <template>
@@ -145,7 +198,7 @@
                         type="link"
                         @click="showContactDetail(record, 'info')"
                     >
-                        {{ record[column.dataIndex] }}
+                        <span class="full-name">{{ record[column.dataIndex] }}</span>
                     </a-button>
                 </template>
                 <template v-if="column.dataIndex === 'phones'">
@@ -159,13 +212,60 @@
                 <template v-if="column.dataIndex === 'channels'">
                     <Channels :items="record.channels" />
                 </template>
+                <template v-if="column.dataIndex === 'queues'">
+                    <ContactQueues :items="record.queues" />
+                </template>
+                <template v-if="column.dataIndex === 'last_management_date'">
+                    {{ record.last_management_date ? getDate(record.last_management_date) : '-' }}
+                </template>
+                <template v-if="column.dataIndex === 'last_interaction_date'">
+                    {{ record.last_interaction_date ? getDate(record.last_interaction_date) : '-' }}
+                </template>
+                <template v-if="column.dataIndex === 'created_at'">
+                    {{ record.created_at ? getDate(record.created_at) : '-' }}
+                </template>
+                <template v-if="column.dataIndex === 'updated_at'">
+                    {{ record.updated_at ? getDate(record.updated_at) : '-' }}
+                </template>
                 <template v-if="column.key === 'action'">
-                    <a-flex gap="small" justify="center">
+                    <a-flex gap="small" justify="center" align="center">
                         <a-switch size="small" />
-                        <DeleteOutlined color="primary" />
+                        <a-button type="link" @click="showDeleteContactModal(record.id, record.full_name)">
+                            <DeleteOutlined />
+                        </a-button>
                     </a-flex>
                 </template>
             </template>
         </STable>
+        <ContactDetailDrawer
+            v-if="contactDetail"
+            v-model="showContactDetailModal"
+            :detail="contactDetail"
+            v-model:tab="activeContactDetailTab"
+            @prev="onPreviousDetailContact"
+            @next="onNextDetailContact"
+            :disabled-prev="contactDetailIndex <= 0"
+            :disabled-next="contactDetailIndex >= (props.data.length - 1)"
+        />
+        <contextHolder />
     </div>
 </template>
+<style lang="scss" scoped>
+.full-name{
+    width: 200px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    text-align: start;
+}
+</style>
+<style lang="scss">
+.confirm-notification{
+    .ant-message-notice-content{
+        color: #595959;
+        font-size: 14px;
+        border: 1px solid #B7EB8F;
+        background-color: #F6FFED;
+    }
+}
+</style>
