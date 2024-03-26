e94ffca80d51a790a0520168a49fc0fa7c658011
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2024-03-26 14:41:38 -0500

    feat: load data with filter params

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,76 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
+  import { ref, watch, computed } from 'vue'
   import ContactFilter from '@/components/contact/ContactFilter.vue'
   import ContactDatatable from '@/components/contact/ContactDatatable.vue'
   import CreateContactModal from '@/components/contact/modals/CreateContactModal.vue'
   import { PlusOutlined } from '@ant-design/icons-vue'
   import { ContactService } from '@/services/contact.service'
-  import type { ContactModule } from '@/interfaces/IContact'
+  import type { ContactModule, ContactAdvancedFilters } from '@/interfaces/IContact'
 
   const contactService = new ContactService()
 
   const moduleData = ref<ContactModule>()
-
+  const loadingData = ref(false)
+  const entityId = ref(0)
+  const campaignId = ref(0)
+  const searchText = ref('')
+  const contactParams = computed(() => ({
+      entity_id: entityId.value || undefined,
+      portfolio_id: campaignId.value || undefined,
+      search: searchText.value || undefined,
+      ...advancedFilters.value
+    }))
   const showCreateContactModal = ref(false)
+  
+  const advancedFilters = ref<ContactAdvancedFilters>({})
+  function onApplyAdvancedFilters(filters: ContactAdvancedFilters){
+    advancedFilters.value = filters
+    initData()
+  }
 
   async function initData () {
-    moduleData.value = await contactService.getContacts()
+    try {
+      loadingData.value = true
+      moduleData.value = await contactService.getContacts(contactParams.value)
+      loadingData.value = false
+    } catch (error) {
+      loadingData.value = false
+      console.error('error getContacts')
+    }
   }
 
   initData()
+  
+  watch(contactParams, () => {
+    initData()
+  })
 </script>
 
 <template>
   <a-layout class="layout">
     <a-layout-content>
-      <div>
+      <div style="margin-bottom: 16px;">
         <div class="header">
           <span class="text">Gestiona los contactos de tus campañas. Puedes ver, editar información y realizar acciones individuales como llamadas. Click aquí para conocer más.</span>
           <span><b>Contactos encontrados:</b> {{ moduleData?.count }}</span>
         </div>
-        <ContactFilter />
-        <a-button
-          @click="showCreateContactModal = true"
-        >
-          Crear contacto <PlusOutlined />
-        </a-button>
+        <a-flex justify="space-between" style="margin-top: 16px;">
+          <ContactFilter
+            v-model:entity="entityId"
+            v-model:campaign="campaignId"
+            v-model:search="searchText"
+            @apply-advanced-filter="onApplyAdvancedFilters"
+          />
+          <a-button
+            @click="showCreateContactModal = true"
+            class="add-btn"
+          >
+            Crear contacto <PlusOutlined />
+          </a-button>
+        </a-flex>
       </div>
-      <ContactDatatable v-if="moduleData" :data="moduleData.results" />
-      <CreateContactModal v-model="showCreateContactModal" />
+      <ContactDatatable :data="moduleData?.results ?? []" :loading-data="loadingData" />
+      <CreateContactModal v-model="showCreateContactModal" @refresh="initData" />
     </a-layout-content>
   </a-layout>
 </template>
@@ -48,6 +82,10 @@
     display: flex;
     justify-content: space-between;
     font-size: 14px;
+  }
+  .add-btn{
+    color: #1890FF;
+    border: 1px solid #1890FF;
   }
 }
 </style>
