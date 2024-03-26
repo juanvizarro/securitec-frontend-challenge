af7cee7c3a608a437b13d012833bede64223955a
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2024-03-26 08:41:32 -0500

    feat: update contact service

--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -1,5 +1,5 @@
 import { api } from "@/api"
-import { ContactModule } from "@/interfaces/IContact"
+import type { ContactModule, ContactDetail, ContactFormState } from "@/interfaces/IContact"
 
 const SERVICE_PREFIX = 'contacts/'
 
@@ -7,5 +7,25 @@ export class ContactService {
     public async getContacts(){
         const { data } = await api.get<ContactModule>(`${SERVICE_PREFIX}clients`)
         return data
+    }
+    public async getContactDetail(id: number){
+        const { data } = await api.get<ContactDetail>(`${SERVICE_PREFIX}clients/${id}`)
+        return data
+    }
+    public async deleteContact(id: number){
+        try {
+            await api.delete(`${SERVICE_PREFIX}clients/${id}`)
+            return true
+        } catch (error) {
+            console.error('error in service: deleteContact')
+            return false
+        }
+    }
+    public async createContact(data: ContactFormState){
+        try {
+            const response = await api.post(`${SERVICE_PREFIX}clients/create`, data)
+        } catch (error) {
+            console.error('error in service: createContact')
+        }
     }
 }
